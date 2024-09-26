064e92eb1fa103b8cc382c77c67cea83d4b00fd0
satisshhhhhh <satishpanchal651@gmail.com>
2024-09-26 18:33:55 +0530

    Fixed the Links and the single quote

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -15,7 +15,7 @@ export const ContactSection = () => {
           <div className="flex flex-col md:flex:row gap-2 items-center">
             <div>
               <h2 className="font-serif text-2xl md:text-3xl">
-                {"Let&apos;s create something together"}
+                Let&apos;s create something together
               </h2>
               <p className="text-sm mt-2 md:text-base">
                 Ready to bring your next project to life? Let&apos;s connect and
